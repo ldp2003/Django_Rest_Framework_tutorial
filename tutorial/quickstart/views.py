a4822aab9977a3c8dda1c23eb3d50bb1e398ab24
ldp2003 <phatpro1208@gmail.com>
2025-07-18 17:26:36 +0700

    extra action, pagination

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes, action
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework.pagination import PageNumberPagination
 
 from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 from .models import Product
@@ -25,16 +26,29 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+class ProductPagination(PageNumberPagination):
+    page_size = 10 
+    page_size_query_param = 'size' #custom params truyền vào để request số lượng trên 1 trang
+    max_page_size = 10 #giới hạn số lượng trên 1 trang 
+
 class ProductViewSet(viewsets.ModelViewSet):
     """
     Viewset specify cho admin 
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
     permission_classes = [permissions.AllowAny] #set tạm allow any
     # permission_classes = [IsAdminUser]
 
-
+    #demo extra action
+    @action(detail=True, methods=['put'], permission_classes=[permissions.AllowAny]) # để tạm allow any do chưa biết test với login trong postman
+    def update_price(self, request, pk=None):
+        product = self.get_object()
+        new_price = request.data.get('price')
+        product.price = new_price
+        product.save()
+        return Response({'message': 'price updated'})
 
 # decorator tạo endpoint cho function views based hoặc không phải class-based viewset
 @api_view()
